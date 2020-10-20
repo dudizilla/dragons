2e61d6a4f3f0fbfcfaa0bb60b1792b64616e1ddb
dudizilla <eduarda.mcunha@outlook.com>
2020-10-20 03:05:38 -0300

    updating gitignore and index.js

--- dragons/src/index.js
+++ dragons/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Home from './Home';
-import * as serviceWorker from './serviceWorker';
+import HomeUnauthenticated from './Unauthenticated';
+import HomeAuthenticated from './Authenticated';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
+  <Auth0Provider
+  domain={domain}
+  clientId={clientId}
+  redirectUri={window.location.origin}>
+    <HomeUnauthenticated />
+    <HomeAuthenticated />
+  </Auth0Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
