497d6226ec2e889988cbf68f9a7577a39a59028c
dudizilla <eduarda.mcunha@outlook.com>
2020-10-21 21:44:07 -0300

    removing comments and fix indentation

--- dragons/src/components/login-button/LoginButton.js
+++ dragons/src/components/login-button/LoginButton.js
@@ -4,15 +4,15 @@ import { useAuth0 } from '@auth0/auth0-react';
 import './LoginButton.css'
 
 const LoginButton = () => {
-    const { loginWithRedirect, isAuthenticated } = useAuth0();
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
 
-    return (
-        !isAuthenticated && (
-        <button className="login-button" onClick={() => loginWithRedirect()}>
-            Login with Auth0
-        </button>
-        )
+  return (
+    !isAuthenticated && (
+      <button className="login-button" onClick={() => loginWithRedirect()}>
+        Login with Auth0
+      </button>
     )
+  )
 }
 
 export default LoginButton
